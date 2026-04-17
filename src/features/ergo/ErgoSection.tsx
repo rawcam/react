6b04235bf7bf6960b98127de68f33d45b9d60757
rawcam <rawcam@ya.ru>
2026-04-17 14:39:08 +0300

    Create ErgoSection.tsx

---
+++ src/features/ergo/ErgoSection.tsx
@@ -0,0 +1,21 @@
+// src/features/ergo/ErgoSection.tsx
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setViewMode, setActiveCalculator } from '../../store/tractsSlice';
+
+export const ErgoSection: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const openCalculator = () => {
+    dispatch(setActiveCalculator('ergo'));
+    dispatch(setViewMode('calculator'));
+  };
+
+  return (
+    <div className="section-content-inner">
+      <button className="mode-btn" onClick={openCalculator}>
+        <i className="fas fa-calculator"></i> Эргономика ЭКП
+      </button>
+    </div>
+  );
+};
