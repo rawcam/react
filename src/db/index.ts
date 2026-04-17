da61b6a7d7d3f1f1ce076a178e70c7d101024350
rawcam <rawcam@ya.ru>
2026-04-17 12:58:44 +0300

    Create index.ts

---
+++ src/db/index.ts
@@ -0,0 +1,22 @@
+// src/db/index.ts
+import Dexie, { Table } from 'dexie';
+import { Project } from '../store/projectsSlice';
+import { CompanyExpense } from '../store/companyExpensesSlice';
+import { ServiceVisit } from '../store/serviceVisitsSlice';
+
+export class SputnikDB extends Dexie {
+  projects!: Table<Project>;
+  companyExpenses!: Table<CompanyExpense>;
+  serviceVisits!: Table<ServiceVisit>;
+
+  constructor() {
+    super('SputnikDB');
+    this.version(1).stores({
+      projects: 'id, shortId, category, status, engineer, projectManager',
+      companyExpenses: 'id, date, category',
+      serviceVisits: 'id, projectId, date, status',
+    });
+  }
+}
+
+export const db = new SputnikDB();
