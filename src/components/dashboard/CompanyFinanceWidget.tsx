1aeb06f99c03db0310a63acbe6d599e4ecd5ec19
rawcam <rawcam@ya.ru>
2026-04-21 15:30:51 +0300

    Update CompanyFinanceWidget.tsx

--- src/components/dashboard/CompanyFinanceWidget.tsx
+++ src/components/dashboard/CompanyFinanceWidget.tsx
@@ -26,7 +26,7 @@ export const CompanyFinanceWidget: React.FC = () => {
     return () => document.removeEventListener('mousedown', handleClickOutside);
   }, []);
 
-  if (!hasRole('director') && !hasRole('pm')) return null;
+  if (!hasRole(['director', 'pm'])) return null;
 
   const formatCurrency = (value: number) => {
     return new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB', minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(value);
