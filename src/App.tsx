783b5724077fd62065f283d2574db709cb916d73
rawcam <rawcam@ya.ru>
2026-04-27 11:35:30 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -34,12 +34,12 @@ const clearStorage = () => {
 };
 clearStorage();
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_PROXY_URL || import.meta.env.VITE_SUPABASE_URL;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    autoRefreshToken: true, // ← Supabase обновляет токен сам
+    autoRefreshToken: true,
   },
 });
 
